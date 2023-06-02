5f0330d40876751e6d34d51c68045f7c4a3ad87f
John <babitskiye@gmail.com>
2023-06-02 07:02:23 +0300

    Добавьте файлы проекта.

---
+++ Models/DataWorker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageStaff.Models
+{
+    public static class DataWorker
+    {
+        // создать отдел
+
+        // создать позицию
+
+        // создать сотрудника
+
+        // удалить отдел
+
+        // удалить позицию
+
+        // удалить сотрудника
+
+        // изменить отдел
+
+        // изменить позицию
+
+        // изменить сотрудника
+    }
+}
